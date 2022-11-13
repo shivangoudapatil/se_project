95d5be0deb1eb5d260ce346ad3c75d12b3a6f9f8
Varun-V-Avabratha <113993203+Varun-V-Avabratha@users.noreply.github.com>
2022-11-13 17:09:02 +0530

    Add files via upload

---
+++ codes/validateC.py
@@ -0,0 +1,19 @@
+import os
+
+def validateProgramType(fileName):
+    ext = os.path.splitext(fileName)[-1].lower()
+    if(ext==".c"):
+        print("Valid File Type")
+        return True
+    else:
+        print("Not a valid file type")
+        return False
+
+
+x = validateProgramType("hello.c")
+x1=validateProgramType("prog1.c")
+y = validateProgramType("hi.cpp")
+y1 = validateProgramType("prog2c.py")
+
+assert(x==True)
+assert(y==False)
