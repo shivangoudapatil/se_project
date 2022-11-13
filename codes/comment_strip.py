94c3e319d978cb3de57f0700f3758886fcbab27f
Shivangouda Patil <91386977+shivangoudapatil@users.noreply.github.com>
2022-11-13 21:47:58 +0530

    Delete codes directory
    uploading new version with lot of bug fixes

--- codes/comment_strip.py
+++
@@ -1,3 +0,0 @@
-import re
-def comment_strip(txt):
-	return re.sub('//.*?\n|/\*.*?\*/', '', txt, flags=re.S)
--- codes/plagcheck.py
+++
@@ -1,4 +0,0 @@
-def plagcheck(txt1,txt2):
-	txt1 = txt1.lower();
-	txt2 = txt2.lower();
-	return 0.53
