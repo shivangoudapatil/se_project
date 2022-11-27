18e2f198f56419170dfc0d43c5f11d27df989d4c
Shivangouda Patil <91386977+shivangoudapatil@users.noreply.github.com>
2022-11-27 11:25:48 +0530

    Update getplag.py

--- codes/getplag.py
+++ codes/getplag.py
@@ -1,4 +1,28 @@
 def getplag(txt1,txt2):
+
+	if len(txt1)<len(txt2): txt1,txt2 = txt2,txt1
+	
 	txt1 = txt1.lower();
 	txt2 = txt2.lower();
-	return 0.53
+	
+	mx = len(txt1)
+	mn = len(txt2)
+	total = (mn*(mn+1))/2
+	cur = mx
+    		
+	dp = [[0 for i in range(0,mx)]]
+	
+	plag = 0
+	
+	for i in range(0,mn):
+		tmp = [0]
+		for j in range(0,mx):
+			if txt2[i]==txt1[j]: 
+			    plag = plag + dp[i][j] + 1
+			    tmp.append(dp[i][j]+1)
+			else: tmp.append(0)
+		dp.append(tmp)
+	
+	ans = min(plag/total,1)
+	
+	return round(ans,2)
