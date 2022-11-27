e831b1608b6b98caf1fc5515f2fa3c48dc49afff
Shivangouda Patil <91386977+shivangoudapatil@users.noreply.github.com>
2022-11-27 11:52:25 +0530

    Update getplag.py

--- codes/getplag.py
+++ codes/getplag.py
@@ -1,5 +1,7 @@
+#using dynamic programming to store all the common substrings and counting them to predict 
 def getplag(txt1,txt2):
 
+	#we are creating a table horizontally is bigger string and vertically smaller
 	if len(txt1)<len(txt2): txt1,txt2 = txt2,txt1
 	
 	txt1 = txt1.lower();
@@ -7,11 +9,15 @@ def getplag(txt1,txt2):
 	
 	mx = len(txt1)
 	mn = len(txt2)
+	
+	#if the strings were same this will be total number of common substrings
 	total = (mn*(mn+1))/2
 	cur = mx
     		
+	#2d array, (first array is all 0's)
 	dp = [[0 for i in range(0,mx)]]
 	
+	#will count total number of common substrings along with length
 	plag = 0
 	
 	for i in range(0,mn):
