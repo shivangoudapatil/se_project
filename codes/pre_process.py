c43927d023f0ee6dad9b9751f4a87304a59dc770
Shivangouda Patil <91386977+shivangoudapatil@users.noreply.github.com>
2022-11-27 11:55:00 +0530

    Update pre_process.py

--- codes/pre_process.py
+++ codes/pre_process.py
@@ -2,14 +2,21 @@ import os
 from comment_strip import comment_strip
 from remove_variables import remove_variables
 from definitions import ROOT_DIR
+
+#if its not c file we will not consider it
 from validateC import validateProgramType
+
 def pre_process():
+	
 	diri = os.fsencode(os.path.join(ROOT_DIR, 'input_files'))
 	diro = os.fsencode(os.path.join(ROOT_DIR, 'processed_files'))
 	dirs = os.fsencode(os.path.join(ROOT_DIR, 'semi_processed_files'))
 	diri = diri.decode('utf-8')
 	diro = diro.decode('utf-8')
 	dirs = dirs.decode('utf-8')
+	
+	
+	#validating and removing comment
 	for file in os.listdir(diri):
 		with open(os.path.join(diri,file),'r') as f:
 			if validateProgramType(file):
@@ -17,9 +24,12 @@ def pre_process():
 				txt = comment_strip(txt)
 				with open(os.path.join(dirs,file),'w') as ff:
 					ff.write(txt)
+	
+	#properly formatting / beautifying c code to maintain uniformity
 	for file in os.listdir(dirs):
 		os.system("clang-format -style=Microsoft -i " + os.path.join(dirs,file))
-			
+		
+	#removing variables in every file in directory
 	for file in os.listdir(dirs):
 		with open(os.path.join(dirs,file),'r') as f:
 			txt = f.read()
