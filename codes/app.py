65058ac9b8f9bae065d09c2c39b0d4651cfde08c
Shivangouda Patil <91386977+shivangoudapatil@users.noreply.github.com>
2022-11-27 11:25:23 +0530

    Update app.py

--- codes/app.py
+++ codes/app.py
@@ -1,4 +1,4 @@
-import os
+import os,subprocess
 from definitions import ROOT_DIR
 from getplag import getplag
 from pre_process import pre_process
@@ -7,7 +7,7 @@ def check():
 	pre_process()
 	dirr = os.fsencode(os.path.join(ROOT_DIR, 'processed_files'))
 	dirr = dirr.decode('utf-8')
-	ot_mat = []
+	ot_mat = [["filename1","filename2","plagiarism result %"]]
 	for file1 in os.listdir(dirr):
 		with open(os.path.join(dirr,file1),'r') as f1:
 			t1 = f1.read()
@@ -20,7 +20,12 @@ def check():
 					if ff1>ff2: ff1,ff2 = ff2,ff1
 					lst.append(ff1)
 					lst.append(ff2)
-					lst.append(getplag(t1,t2))
+					lst.append(getplag(t1,t2)*100)
 				ot_mat.append(lst)
-	print(ot_mat)
+				
+	with open(os.path.join(ROOT_DIR, 'results.csv'),"w") as ff:
+		b = [','.join(str(x) for x in ele) for ele in ot_mat]
+		st = '\n'.join(b)
+		ff.write(st)
+	subprocess.call(('xdg-open',os.path.join(ROOT_DIR, 'results.csv')))
 check()
