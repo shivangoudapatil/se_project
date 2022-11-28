0c6e4a2b55ee5991fdbac9e03dd1e2e8d8e41235
Sathyajeeth R <90851507+sathyajee@users.noreply.github.com>
2022-11-28 19:21:05 +0530

    Final_update

---
+++ codes/app.py
@@ -0,0 +1,63 @@
+#this is for opening files and imitate running various commands on terminal
+import os,subprocess
+
+#this makes our code work in any environment / folder
+from definitions import ROOT_DIR 
+
+#this will take two strings as input and output the plagiarism percentage
+from getplag import getplag  
+
+#this will remove comments and variables then convert c files to proper format
+from pre_process import pre_process 
+
+#this is used to delete files temp files after obtaining results
+from delete_files import delete_temp_files
+
+def check():
+	
+	##this will remove comments and variables then convert c files to proper format
+	pre_process() 
+	
+	#directory to store processed files
+	dirr = os.fsencode(os.path.join(ROOT_DIR, 'processed_files'))
+	dirr = dirr.decode('utf-8')
+	
+	#heading of our csv file
+	ot_mat = [["filename1","filename2","plagiarism result %"]]
+	
+	filePair=[]
+
+	#checking each file with every other file for plagiarism
+	for file1 in os.listdir(dirr):
+		with open(os.path.join(dirr,file1),'r') as f1:
+			t1 = f1.read()
+			for file2 in os.listdir(dirr):
+				lst = []
+				with open(os.path.join(dirr,file2),'r') as f2:
+					t2 = f2.read()
+					ff1 = file1[0:len(file1)-2]
+					ff2 = file2[0:len(file2)-2]
+					
+					#smaller file comes in first column in the csv file
+					if ff1>ff2: ff1,ff2 = ff2,ff1 
+					newPair=[ff1,ff2]
+					if newPair in filePair:
+						continue
+					lst.append(ff1)
+					lst.append(ff2)
+					lst.append(getplag(t1,t2)*100)
+					filePair.append(newPair)
+				ot_mat.append(lst)
+	
+	#writing the 2d matrix os_mat to the csv file in proper format
+	with open(os.path.join(ROOT_DIR, 'results.csv'),"w") as ff:
+		b = [','.join(str(x) for x in ele) for ele in ot_mat]
+		st = '\n'.join(b)
+		ff.write(st)
+		
+	#deleting all temp files created
+	delete_temp_files()
+		
+	#opening the csv file with default app
+	subprocess.call(('xdg-open',os.path.join(ROOT_DIR, 'results.csv')))
+check()
